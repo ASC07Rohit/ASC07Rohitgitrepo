80da8a4f5c3797062790e3cc19638326da144cc3
Rohit Selokar <rohit.selokar@ascendion.com>
2024-10-18 12:40:50 +0530

    training

--- a1-typescript-product-manager/src/products.ts
+++ a1-typescript-product-manager/src/products.ts
@@ -16,7 +16,3 @@ export interface Product{
 
 // releaseDate: string; releaseDate: string; releaseDate: string; releaseDate: string;
 // releaseDate: string; releaseDate: string; releaseDate: string; releaseDate: string;
-// releaseDate: string; releaseDate: string; releaseDate: string; releaseDate: string;
-// releaseDate: string; releaseDate: string; releaseDate: string; releaseDate: string;
-// releaseDate: string; releaseDate: string; releaseDate: string; releaseDate: string;
-// releaseDate: string; releaseDate: string; releaseDate: string; releaseDate: string;
